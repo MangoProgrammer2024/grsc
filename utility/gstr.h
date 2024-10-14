1771d41c250e826eef0ab75175590d087a540220
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-14 10:54:55 -0500

    Create gstr.h

---
+++ utility/gstr.h
@@ -0,0 +1,26 @@
+/*
+* *gstr.h* 
+* *level string*
+*/
+#pragma once
+
+#define STRING_REFERENCE 010
+#define STRING_LINE 020
+
+#endif
+
+#include <string>
+#include <cstdio>
+#include <math.h>
+
+struct REFERENCE{
+const std::string local_reference(REFERENCE&)const;
+
+std::string _Refer__to_ *localized_string;
+std::FILE * GStr;
+
+#define REFERENCE_LOCAL_STRING_SIZE 64x64
+
+const char *REFERENCE::local_reference(reference_buffer(REFERENCE_LOCAL_STRING_SIZE))const;
+
+};
