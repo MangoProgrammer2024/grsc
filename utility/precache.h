1ed43b204c7e68885750124e49c39e93c55615bb
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-14 10:16:43 -0500

    Create precache.h

---
+++ utility/precache.h
@@ -0,0 +1,20 @@
+/*
+*  *precache.h*
+*/
+
+#include "utility.h"
+
+typedef std::string precache;
+typedef bool cachetrue;
+typedef bool cachefalse;
+
+typedef struct Cache{
+
+Cache cache;
+
+  void insertprecache(Cache(precache));
+  void removeprecache(Cache(precache));
+
+float cacherun;
+
+};
