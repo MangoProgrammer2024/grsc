5220dc646a344874d736fdeffa4a6d82955ba505
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-16 10:01:40 -0500

    Create common_utility.hpp

---
+++ utility/common_utility.hpp
@@ -0,0 +1,19 @@
+/*
+*  ***common_utility.hpp***
+*/
+#pragma once
+
+#ifndef COMMON_UTILITY_HPP
+#define COMMON_UTILITY_HPP
+
+/*this file is used for printing messages to consoles and logs for the game engine*/
+
+#include <string>
+
+void printLn(const char * lnName, std::string * ln);
+void printConsole(auto Console, const char * current_console, *msg);
+void printLog(auto txtLog, const char * current_log, *txt);
+void printChar(typedef char CHAR);
+
+#endif
+
