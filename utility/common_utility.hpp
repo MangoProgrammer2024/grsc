9d00f44c7f118a7c3e2408fd71793e7a11debc1a
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-16 12:22:41 -0500

    Update common_utility.hpp

--- utility/common_utility.hpp
+++ utility/common_utility.hpp
@@ -13,10 +13,22 @@
 char * txt[1024];
 static char txtBuff[2048];
 
-void printLn(const char * lnName, std::string * ln);
+void printLn(const char * lnName, std::string *ln);
 void printConsole(auto Console, const char * current_console, *msg);
 void printLog(auto txtLog, const char * current_log, *txt);
 void printChar(typedef char CHAR);
+
+auto BufferText;
+
+int VectorScalePoints[3];
+void printVector(const char * message = "vector printed to brushes\n");
+
+void PrintMsgLoop(){
+ for(int i = 0; i > 0; i++)
+   {
+      void& printableMessage();
+   }
+};
 
 #endif
 
