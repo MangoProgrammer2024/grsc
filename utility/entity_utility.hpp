478c649659cae3e9f42c958c6606164116a4ea54
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-16 12:33:10 -0500

    Update entity_utility.hpp

--- utility/entity_utility.hpp
+++ utility/entity_utility.hpp
@@ -2,4 +2,30 @@
 *     *entity_utility.hpp*
 *     *this file is used for handling world entities*
 */
+#pragma once 
+
+#define WORLD_ENTITY
+
+#include "utility.hpp"
+#include "common_utility.hpp"
+#include <string>
+
+std::string * Key;
+std::string * value;
+
+const char * world_classname;
+
+std::string getent;
+
+typedef class worldEntity{
+public:
+
+  void getkey(worldEntity e, std::string * key);
+  void getvalue(worldEntity e, std::string * value);
+
+};
+
+
+
+
 
